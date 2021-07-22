15f3e32058d432a2318f864960a08e4f6f65affc
SahilPrajapat <sahil9873.sk@gmail.com>
2021-07-22 15:48:12 +0530

    update

--- levelup/Graph/bfs_question.java
+++ levelup/Graph/bfs_question.java
@@ -299,4 +299,44 @@ public class bfs_question {
         return ans;
     }
 
+
+    public int longestIncreasingPath(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        LinkedList<Integer> que = new LinkedList<>();
+        
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        
+        int[][] indegre = new int[n][m];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++){
+                for(int d = 0; d < 4; d++){
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
+                    
+                    if(r >= 0 && c >= 0 && r < n && c < m && matrix[i][j] > matrix[r][c])
+                        indegre[i][j]++;
+                }
+                
+                if(indegre[i][j] == 0)
+                    que.addLast(i * m + j);
+            }
+        
+        int level = 0;
+        while(que.size() != 0){
+            int size = que.size();
+            while(size-- > 0){
+                int rvtx = que.removeFirst();
+                int sr = rvtx/m, sc = rvtx%m;
+                for(int d = 0; d < 4; d++){
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+                    
+                    if(r >= 0 && c >= 0 && r < n && c < m && matrix[sr][sc] < matrix[r][c] && --indegre[r][c] == 0)
+                        que.addLast(r * m + c);
+                }
+            }
+            level++;
+        }
+        return level;
+    }
 }
--- levelup/Graph/l001.java
+++ levelup/Graph/l001.java
@@ -216,37 +216,4 @@ public class l001 {
         return res;
     }
 
-
-    public static ArrayList<Integer> kahnsAlgo(ArrayList<Edge>[] graph){
-        int N = graph.length;
-        LinkedList<Integer> que = new LinkedList<>();
-        ArrayList<Integer>ans = new ArrayList<>();
-        int[] indegree = new int[N];
-
-        for(int i = 0; i < N; i++){
-            for(Edge e : graph[i])
-                indegree[e.v]++;
-        }
-
-        for(int i = 0; i < N; i++){
-            if(indegree[i] == 0)
-                que.addLast(i);
-        }
-
-        while(que.size() != 0){
-            int rvtx = que.removeFirst();
-            ans.add(rvtx);
-
-            for(Edge e : graph[rvtx]){
-                if(--indegree[e.v] == 0)
-                    que.addLast(e.v);
-            }
-        }
-
-        if(ans.size() != N)
-            ans.clear();;
-
-        return ans;
-    }
-
 }
